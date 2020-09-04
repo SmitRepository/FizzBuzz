7f5e6ca7df40d60aff52977a7911af19ae8669b1
Smit <Smit@LAPTOP-K4CJ3BVC>
2020-09-04 19:09:59 +0530

    Satisfied 1st Requirement

--- FizzBuzz/src/FizzBuzzTest.java
+++ FizzBuzz/src/FizzBuzzTest.java
@@ -27,4 +27,6 @@ public class FizzBuzzTest {
 		String actualResult = fizzbuzz.check(number);
 		assertEquals("Buzz", actualResult);
 	}
+	
+	
 }
---
+++ FizzBuzz/src/MainClass.java
@@ -0,0 +1,12 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		FizzBuzz obj = new FizzBuzz();
+		for(int i = 1; i<=100; i++) {
+			System.out.println(obj.check(i));
+		}
+
+	}
+
+}
