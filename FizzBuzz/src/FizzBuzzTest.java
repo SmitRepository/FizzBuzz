0d8fb54b2345b3d99066749eaa19864e592a6a4e
Smit <Smit@LAPTOP-K4CJ3BVC>
2020-09-04 18:49:54 +0530

    Fizz Functionality added

--- FizzBuzz/src/FizzBuzzTest.java
+++ FizzBuzz/src/FizzBuzzTest.java
@@ -1,3 +1,5 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
@@ -5,5 +7,16 @@ public class FizzBuzzTest {
 	@Test
 	public void testNumber() {
 		FizzBuzz fizzbuzz = new FizzBuzz();
+		int number = 1;
+		String actualResult = fizzbuzz.check(number);
+		assertEquals("1", actualResult);
+	}
+	
+	@Test
+	public void testFizz(){
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		int number = 3;
+		String actualResult = fizzbuzz.check(number);
+		assertEquals("Fizz", actualResult);
 	}
 }
